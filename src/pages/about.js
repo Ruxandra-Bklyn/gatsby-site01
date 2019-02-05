e908be86f81ff7ff17b6cc66ae3a6860d439c7c5
ruxandra vasilescuE <ruxilescu@gmail.com>
2019-02-05 00:30:33 -0500

    Enclosed about page content in div element

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,18 +1,21 @@
- import React from 'react'
-import { Link } from 'gatsby'
-import Layout from '../components/layout'
-import SEO from '../components/seo'
+import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
 const AboutPage = () => (
 <Layout>
+  <div>
     <h1>About Me</h1>
     <h2>I need more here</h2>
     <p>
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nostrum consequatur sed illum velit est accusamus, quae fuga autem, sunt, eius assumenda tempora sint laborum commodi praesentium officia quasi maxime!
     </p>
+    <p>Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nostrum consequatur sed illum velit est accusamus, quae fuga autem, sunt, eius assumenda tempora sint laborum commodi praesentium officia quasi maxime!</p>
    <Link to="/">Go back to the Home page</Link>
    <br/>
    <Link to="/contact">Go to Contact page</Link>
+   </div>
   </Layout> 
 )
 
